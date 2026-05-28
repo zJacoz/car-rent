287359c4d335d406967ef971af7e129a06e67373
zJacoz <jaco.net91@gmail.com>
2026-05-28 11:20:10 -0300

    including rental and taxservice

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,23 @@
+package model.services;
+
+import model.entities.CarRental;
+
+public class RentalService {
+	
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+	}
+	
+	
+}
