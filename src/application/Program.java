4613facd2e7b3df02b6892dc02848fb1a6234092
zJacoz <jaco.net91@gmail.com>
2026-06-09 09:53:55 -0300

    Finished no interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -15,17 +17,30 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
 		System.out.println("Entre com os dados do aluguel:");
 		System.out.print("Modelo do carro: ");
 		String carModel = sc.nextLine();
-		System.out.print("Retirada (dd/MM/yyyy): ");
+		System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.print("Devolução (dd/MM/yyyy): ");
+		System.out.print("Devolução (dd/MM/yyyy HH:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		rentalService.processInvoice(cr);
+		
+		System.out.println("FATURA:");
+		System.out.println("Pagamento básico: " + cr.getInvoice().getBasicPayment());
+		System.out.println("Imposto: " + cr.getInvoice().getTax());
+		System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
 		
 		sc.close();
 	}
--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,6 +1,9 @@
 package model.services;
 
+import java.time.Duration;
+
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalService {
 	
@@ -17,6 +20,20 @@ public class RentalService {
 	
 	public void processInvoice(CarRental carRental) {
 		
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		} 
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
 	}
 	
 	
